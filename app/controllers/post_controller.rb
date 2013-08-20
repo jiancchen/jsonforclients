978b1ef9bd8e7ec1bbee8c8181f6d0345d1ea0be
Jian Chen <quantumsix492@gmail.com>
2013-08-20 15:58:17 -0700

    adding controller and model

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,23 @@
+class PostController < ApplicationController
+
+ skip_before_filter :verify_authenticity_token  
+
+  def new
+  	@post = Post.new()
+  end
+
+  def create
+  	@post = Post.new(user_params)
+  	@post.save
+  	respond_to do |format|
+  		format.html # index.html.erb
+   		format.json { render json: @post }
+	end
+
+  end
+
+  private
+	  def user_params
+	    params.require(:post).permit(:title)
+	  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Jsonforclients::Application.routes.draw do
+  resources :post
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
